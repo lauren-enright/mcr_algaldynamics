5d6ae824c07e0fea4f9efa9ab302a3e2015b1807
CallieStephenson <callies@hawaii.edu>
2024-06-12 09:52:09 -1000

    whoops, uploaded it kind of ugly. this just cleans up Fish_tidy so it is a bit more digestible :fish:

--- R/Fish_tidy.R
+++ R/Fish_tidy.R
@@ -10,30 +10,27 @@ library(dplyr)
 library(stringr) #to make functional fish groups match taxonomy
 library(tidyr)
 
-fish <- read.csv("data/MCR_LTER_Annual_Fish_Survey_20230615.csv") %>% 
-  filter(!Year == "(100840 rows)")
-fish_functional <- read.csv("data/recharge_fish_fun_groups.csv")
+fish <- read.csv("data/MCR_LTER_Annual_Fish_Survey_20230615.csv") %>% #Find this file in google drive, too big for git
+  filter(!Year == "(100840 rows)") #i don't know why it has this but i hate it
+fish_functional <- read.csv("data/recharge_fish_fun_groups.csv") #Also find this in google drive, didn't want it public on git
 
-#I was given the fish_functional sheet for updated trophic levels for some species. but it doesn't have all species
+#I was given the fish_functional sheet for updated trophic levels for some species from recharge, but it doesn't have all species
 #So, I'm going to make a column for functional_group in fish that has either what it is listed as in the recharge sheet
 #and if not, I'm going to have it take the fine_trophic
 
+### Tidy functional sheet to match fine_trophic style: 
 #Capitalize the first letter of the functional group to match style of fine trophic
 fish_functional$functional_group <- str_to_title(fish_functional$functional_group)
-
 #Change the taxonomy to have a capital letter only for Genus (the first letter)
 capitalize_first <- function(x) {
   # Convert the first letter to uppercase and concatenate it with the rest of the string
   paste0(toupper(substring(x, 1, 1)), substring(x, 2))
 }
-
 #make the Taxonomy joining column
 fish_functional$Taxonomy <- sapply(fish_functional$genus_species, capitalize_first)
-
 #Remove NA values and duplicates
 fish_functional_no_na <- fish_functional[!is.na(fish_functional$functional_group), ] %>% 
   distinct(Taxonomy, .keep_all = TRUE)
-
 #change around some weirdly-named functional groups
 fish_functional_no_na_fixed <- fish_functional_no_na %>%
   mutate(functional_group = if_else(functional_group == 'Benthicinvertebrateconsumer', 'Benthic Invertebrate Consumer', functional_group)) %>% 
@@ -42,12 +39,9 @@ fish_functional_no_na_fixed <- fish_functional_no_na %>%
   mutate(functional_group = if_else(functional_group == 'Concealedcropper', 'Concealed Cropper', functional_group)) %>% 
   mutate(functional_group = if_else(functional_group == 'Herbivoredetritivore', 'Herbivore/Detritivore', functional_group))
 
-#Tidy them up:
-  # Perform a left join to combine data frames on 'Taxonomy' column
-combined_df <- left_join(fish, fish_functional_no_na_fixed[,c("herbivore_status", "Taxonomy", "functional_group")], by = "Taxonomy")
 
-# Create the 'functional_group' column
-combined_df <- combined_df %>%
+#Combine them:
+combined_df <- left_join(fish, fish_functional_no_na_fixed[,c("herbivore_status", "Taxonomy", "functional_group")], by = "Taxonomy")%>%
   dplyr::mutate(Functional_group = coalesce(functional_group, Fine_Trophic))
 
 #Ok, so now we need to fix all the species that it didn't know where herbivores before because the herbivore_status was only in the recharge table that only has 116 species...
@@ -67,9 +61,6 @@ biomass_by_herb_status_wide <- cleaned_fish %>%
   pivot_wider(names_from = herbivore_status, values_from = total_biomass) %>% 
   rename('Non-Herbivore Biomass' = N) %>% 
   rename('Herbivore Biomass' = Y)
-
-output_path <- file.path("..", "data", "biomass_herb_summary.csv")
-write.csv(biomass_by_herb_status_wide, output_path, row.names = FALSE, col.names = FALSE)
 
 write_csv(biomass_by_herb_status_wide, here("data", "biomass_herb_summary.csv"))
 write_csv(biomass_by_functional_group_wide, here("data", "biomass_functional_summary.csv"))
