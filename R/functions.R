57252d72bc100495c673c860b0e8ae97031527b7
naltmank <nta8@gatech.edu>
2024-10-25 11:55:40 -0400

    added extract_ranges and filter_ranges function for extracting range of data and filtering for plotting of trends

--- R/functions.R
+++ R/functions.R
@@ -19,6 +19,31 @@ sv_trans <- function(prop, s = 0.000005){
   # and s is a small offset 
 }
 
+# standard error
 se <- function(x){
   sd(x)/sqrt(n())
+}
+
+# function to extract the ranges of the data for plotting
+extract_ranges <- function(df, # a dataframe
+                           group, # grouping variable (e.g. habitat)
+                           columns # columns you want to summarize (e.g. richness, cover, synchrony)
+){
+  df %>% # in your dataframe
+    group_by(across(all_of(group))) %>% 
+    summarise(across(all_of(columns), # summarize the columns in your group
+                     list(min = ~min(.x, na.rm = TRUE), max = ~max(.x, na.rm = TRUE)), # by taking the min/max of them
+                     .names = "{.fn}_{.col}")) # and assigning them to the columns name "min/max_value"
+}
+
+# function for filtering ranges of emtrends to only fit range of actual data
+filter_ranges <- function(trend, # emtrends df created from emmip
+                          range_obj, # output df from extract_ranges() custom function
+                          group, # group over which to summarize (e.g. habitat)
+                          value # value to filter range (e.g. richness)
+){
+  trend %>% # take emtrend
+    left_join(range_obj, by = group) %>% # join with range object
+    filter(.data[[value]] >= .data[[paste0("min_", value)]] & # filter so emtrend only covers range of actual values
+             .data[[value]] <= .data[[paste0("max_", value)]])
 }
