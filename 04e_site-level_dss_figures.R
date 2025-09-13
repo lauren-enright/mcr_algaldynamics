b6352537a3ca596d3d12f7694474cd4b56540c0e
Lauren Enright <enrightl09@gmail.com>
2025-09-13 12:49:20 -0700

    adding supplemental figure 5d,e,f

--- 04e_site-level_dss_figures.R
+++ 04e_site-level_dss_figures.R
@@ -2,6 +2,8 @@ source("04d_dss_models.R")
 library(ggplot2)
 library(ggpubr)
 
+supplemental_tables_tableS5_plot <- read.csv(here::here("data", "supplemental_tables_tableS5_sitelevel.csv"))
+
 #### STABILITY ~ RICHNESS ####
 # SETUP
 rich_stab_emm_site <- emmip(rich_stab_mod_site,
@@ -11,7 +13,7 @@ rich_stab_emm_site <- emmip(rich_stab_mod_site,
 filtered_rich_stab_effects_site <- filter_ranges(rich_stab_emm_site, dss_ranges_site, "habitat", "richness_mean")
 
 # PLOT
-(figure_4d <- 
+(figure_s5a <- 
    ggplot() +
    geom_point(data = diversity_stability_synchrony_site,
               aes(x = richness_mean, y = cover_stability, colour = habitat), size = 1, alpha = 0.5) + 
@@ -21,8 +23,9 @@ filtered_rich_stab_effects_site <- filter_ranges(rich_stab_emm_site, dss_ranges_
    scale_colour_manual(values = habitat_colours) +
    scale_fill_manual(values = habitat_colours) +
    scale_y_continuous(limits = c(0.5, 4.2), breaks = c(1,2,3,4)) +
-   labs(y = "Site-level Stability\n", x = "Taxonomic richness", title = "") +
-   model_themes
+   labs(y = "Site-level Stability", x = "Taxonomic richness", title = "") +
+   model_themes +
+  theme(axis.title.y = element_text(size = 30))
 )
 
 # ggsave(filename = "output/figure_4d.png", figure_4d, height = 10, width = 14)
@@ -36,7 +39,7 @@ rich_stab_emm_site_fg <- emmip(rich_stab_mod_site_fg,
 filtered_rich_stab_effects_site <- filter_ranges(rich_stab_emm_site_fg, dss_ranges_site, "habitat", "functional_richness_mean")
 
 # PLOT
-(figure_4e <- 
+(figure_s5b <- 
     ggplot() +
     geom_point(data = diversity_stability_site,
                aes(x = functional_richness_mean, y = cover_stability, colour = habitat), size = 1, alpha = 0.5) + 
@@ -47,7 +50,7 @@ filtered_rich_stab_effects_site <- filter_ranges(rich_stab_emm_site_fg, dss_rang
     scale_colour_manual(values = habitat_colours) +
     scale_fill_manual(values = habitat_colours) +
     scale_y_continuous(limits = c(0.5, 4.2), breaks = c(1,2,3,4)) +
-    scale_x_continuous(limits = c(2, 4.2), breaks = c(1,2,3,4)) +
+    scale_x_continuous(limits = c(1.75, 4.2), breaks = c(1,2,3,4)) +
     labs(y = "", x = "Functional richness", title = "") +
     model_themes
 )
@@ -63,7 +66,7 @@ synch_stab_emm_site <- emmip(synch_stab_mod_site,
 filtered_synch_stab_effects_site <- filter_ranges(synch_stab_emm_site, dss_ranges_site, "habitat", "synchrony")
 
 # PLOT
-(figure_4f <- 
+(figure_s5c <- 
     ggplot() +
     geom_point(data = diversity_stability_synchrony_site,
                aes(x = synchrony, y = cover_stability, colour = habitat), size = 1, alpha = 0.5) + 
@@ -78,14 +81,100 @@ filtered_synch_stab_effects_site <- filter_ranges(synch_stab_emm_site, dss_range
 )
 # ggsave(filename = "output/figure_4f.png", figure_4f, height = 10, width = 14)
 
-#### PANEL GRAPH ####
-(site_dss <- ggarrange(figure_4d, figure_4e, figure_4f, 
-                       ncol = 3, nrow = 1, 
+
+
+# ggsave(filename = "output/site_dss.png", site_dss, height = 6, width = 18)
+
+#supplemental tables -- emtrends output 
+
+#Site-level taxonomic richness 
+
+supplemental_tables_tableS5_plot %>% 
+  filter(Table == "S5") %>% 
+  filter(Predictor == "Site-level taxonomic-richness") %>% 
+  # order the habitats by distance from shore
+  mutate(Habitat = factor(Habitat, levels = c("Forereef 17m", "Forereef 10m",  "Backreef", "Fringing"))) %>% 
+  ggplot(aes(x = Mean, y = Habitat, color = Habitat)) +
+  geom_point(size = 6) +
+  # add confidence intervals:
+  geom_linerange(aes(xmin = Lower_CI, xmax = Upper_CI), linewidth = 2.5) +
+  scale_colour_manual(values = habitat_colours) +
+  # if you want the 0 reference:
+  geom_vline(xintercept = 0, linetype = "dashed", linewidth = 1, color = "gray") +
+  xlab("Coefficients of site-level\n taxonomic richness") +
+  ylab("") +
+  ggtitle("") +
+  model_themes + 
+  # add letters denoting significance
+  geom_text(aes(x = Upper_CI + 0.05 , y = Habitat, 
+                label = .group), colour = "black", size =8) +
+  theme(legend.position = "none",
+        #axis.text.y = element_text(size = 27, angle = 90, hjust = 0.5, vjust = 0.5),
+        axis.text.y = element_text(size = 20)) -> s5.taxon.d
+
+#Site-level functional richness
+
+supplemental_tables_tableS5_plot %>% 
+  filter(Table == "S5") %>% 
+  filter(Predictor == "Site-level functional richness") %>% 
+  # order the habitats by distance from shore
+  mutate(Habitat = factor(Habitat, levels = c("Forereef 17m", "Forereef 10m",  "Backreef", "Fringing"))) %>% 
+  ggplot(aes(x = Mean, y = Habitat, color = Habitat)) +
+  geom_point(size = 6) +
+  # add confidence intervals:
+  geom_linerange(aes(xmin = Lower_CI, xmax = Upper_CI), linewidth = 2.5) +
+  scale_colour_manual(values = habitat_colours) +
+  # if you want the 0 reference:
+  geom_vline(xintercept = 0, linetype = "dashed", linewidth = 1, color = "gray") +
+  xlab("Coefficients of site-level\n functional richness") +
+  ylab("") +
+  ggtitle("") +
+  model_themes + 
+  # add letters denoting significance
+  geom_text(aes(x = Upper_CI + 0.1 , y = Habitat, 
+                label = .group), colour = "black", size = 8) +
+  theme(legend.position = "none",
+      # axis.text.y = element_blank(),
+       # axis.ticks.y = element_blank(),
+        axis.text.y = element_text(size = 20)) -> s5.functional.e
+
+#Site-level species synchrony
+
+supplemental_tables_tableS5_plot  %>% 
+  filter(Table == "S5") %>% 
+  filter(Predictor == "Site-level species synchrony") %>% 
+  # order the habitats by distance from shore
+  mutate(Habitat = factor(Habitat, levels = c("Forereef 17m", "Forereef 10m",  "Backreef", "Fringing"))) %>% 
+  ggplot(aes(x = Mean, y = Habitat, color = Habitat)) +
+  geom_point(size = 6) +
+  # add confidence intervals:
+  geom_linerange(aes(xmin = Lower_CI, xmax = Upper_CI), linewidth = 2.5) +
+  scale_colour_manual(values = habitat_colours) +
+  # if you want the 0 reference:
+  geom_vline(xintercept = 0, linetype = "dashed", linewidth = 1, color = "gray") +
+  xlab("Coefficients of site-level\n species synchrony") +
+  ylab("") +
+  ggtitle("") +
+  model_themes + 
+  # add letters denoting significance
+  geom_text(aes(x = Upper_CI + 0.4 , y = Habitat, 
+                label = .group), colour = "black", size = 8) +
+  theme(legend.position = "none",
+      # axis.text.y = element_blank(),
+       # axis.ticks.y = element_blank(),
+        axis.text.y = element_text(size = 20)) -> s5.synch.f
+
+# pull all 6 panels together...
+
+(supplmement5_site_dss <- ggarrange(figure_s5a, figure_s5b, figure_s5c, 
+                                    s5.taxon.d, s5.functional.e, s5.synch.f,
+                       ncol = 3, nrow = 2, 
                        common.legend = TRUE,
-                       heights = c(1,1),
-                       widths = c(1,1,1),
+                       heights = c(1.5,1.5),
+                       widths = c(2,2,2),
                        legend = "bottom", 
                        labels = c("a.", "b.", "c.", "d.", "e.", "f."),
                        font.label = list(size = 26, color = "black", face = "plain")))
 
-# ggsave(filename = "output/site_dss.png", site_dss, height = 6, width = 18)
+ggsave(filename = "output/supplement5_site_dss.png", supplmement5_site_dss, height = 30, width = 40)
+
