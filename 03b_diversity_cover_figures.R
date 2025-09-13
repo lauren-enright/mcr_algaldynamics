ad748d7f1827a482615421f761e5b626724cc851
Lauren Enright <enrightl09@gmail.com>
2025-09-12 18:14:00 -0700

    adding in code for supplemental figure S2a (plot level only, not sure where the site level models for cover ~ diversity are living right now

--- 03b_diversity_cover_figures.R
+++ 03b_diversity_cover_figures.R
@@ -10,7 +10,7 @@ library(DHARMa)
 library(ggplot2)
 library(ggpubr)
 
-
+supplemental_tables_tableS2_plot <- read.csv(here::here("data", "supplemental_tables_tableS2_plot.csv"))
 
 #### COVER ~ RICHNESS ####
 # SETUP
@@ -73,3 +73,37 @@ filtered_cover_effects_fg <- filter_ranges(cover_emm_fg, diversity_ranges_quad, 
                        font.label = list(size = 26, color = "black", face = "plain")) )
 
 # ggsave(filename = "output/figure_3v3.png", figure_3, height = 8, width = 16)
+
+#adding supplemental figures
+
+#want taxonomic richness as first panel to match
+
+supplemental_tables_tableS2_plot <- supplemental_tables_tableS2_plot %>%
+  mutate(Predictor = factor(Predictor, 
+                          levels = c("Taxonomic richness", "Functional richness")))
+
+supplemental_tables_tableS2_plot %>% 
+  #filter(Table == "S2") %>% 
+  #filter(`Spatial scale` == "Plot-level") %>% 
+  # order the habitats by distance from shore
+  mutate(Habitat = factor(Habitat, levels = c("Forereef 17m", "Forereef 10m",  "Backreef", "Fringing"))) %>% 
+  ggplot(aes(x = Mean, y = Habitat, color = Habitat)) +
+  geom_point(size = 6) +
+  # add confidence intervals:
+  geom_linerange(aes(xmin = Lower_CI, xmax = Upper_CI), linewidth = 2.5) +
+  facet_wrap(~ Predictor) +
+  scale_colour_manual(values = habitat_colours) +
+  xlab("Coefficient") +
+  ylab("") +
+  ggtitle("(a) Plot-level") +
+  model_themes + 
+  # add letters denoting significance
+  geom_text(aes(x = Upper_CI + 0.03 , y = Habitat, 
+                label = Letter), colour = "black", size = 10) +
+  theme(legend.position = "none",
+          strip.text = element_text(size = 16, face = "bold")  # change size/style
+        ) -> s2.plt.level
+
+#ggsave(filename = "output/figure_s2_plotonly.png", s2.plt.level, height = 10, width = 22)
+
+
