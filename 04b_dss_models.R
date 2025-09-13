994407eb06ab7db4757f32910b4ff3c774106db7
Lauren Enright <enrightl09@gmail.com>
2025-09-12 19:12:50 -0700

    adding in option to read in relevant files, adding tables for habitat comparisons for supplemental figure 4

--- 04b_dss_models.R
+++ 04b_dss_models.R
@@ -8,10 +8,18 @@ library(codyn)
 
 source("04a_calculate_synchrony.R")
 
+#read in data produced in 04a_calculate_synchrony script
+#diversity_stability_synchrony <- read.csv(here::here("data", "full_plot_level_dss.csv")) 
+#diversity_stability_synchrony_site <- read.csv(here::here("data", "diversity_stability_synchrony_site.csv"))
+
+#want habitats to be in order.
+diversity_stability_synchrony$habitat <- factor(diversity_stability_synchrony$habitat,
+                                             levels = c("Fringing", "Backreef", "Forereef 10m", "Forereef 17m"))
+
 #### STABILITY ~ RICHNESS ####
 rich_stab_mod <- glmmTMB(cover_stability~ richness_mean*habitat + (1|site), family = Gamma(link = "log"), data = diversity_stability_synchrony)
 summary(rich_stab_mod)
-Anova(rich_stab_mod)
+car::Anova(rich_stab_mod)
 #                          Chisq Df Pr(>Chisq)    
 # richness_mean         2720.311  1  < 2.2e-16 ***
 # habitat                325.657  3  < 2.2e-16 ***
@@ -19,7 +27,7 @@ Anova(rich_stab_mod)
 hist(residuals(rich_stab_mod)) # looks good
 plot(residuals(rich_stab_mod) ~ fitted(rich_stab_mod)) # heteroskedastic - bring to group
 r.squaredGLMM(rich_stab_mod) # 0.803, 0.808
-performance::r2(rich_stab_mod) # 0.812, 0.816
+performance::r2(rich_stab_mod) # marginal: 0.812, conditional: 0.816
 emtrends(rich_stab_mod, pairwise ~ habitat, var = "richness_mean") # fringing not diff from outer17, backreef not different from outer10
 # $contrasts
 #  contrast                     estimate     SE  df z.ratio p.value
@@ -41,10 +49,9 @@ car::Anova(rich_stab_mod_fg)
 # habitat                           120.732  3  < 2.2e-16 ***
 # functional_richness_mean:habitat   22.582  3  4.935e-05 ***
 
-
 hist(residuals(rich_stab_mod_fg)) # looks fine
 plot(residuals(rich_stab_mod_fg) ~ fitted(rich_stab_mod_fg)) # same as usual 
-performance::r2(rich_stab_mod_fg) # 0.823, 0.832
+performance::r2(rich_stab_mod_fg) # marginal: 0.823, conditional: 0.832
 emtrends(rich_stab_mod_fg, pairwise ~ habitat, var = "functional_richness_mean") 
 #  fringing different from all others
 # contrast                    estimate     SE  df z.ratio p.value
@@ -58,7 +65,7 @@ emtrends(rich_stab_mod_fg, pairwise ~ habitat, var = "functional_richness_mean")
 #### STABILITY ~ SYNCHRONY ####
 synch_stab_mod <- glmmTMB(cover_stability ~ synchrony*habitat + (1|site), family = Gamma("log"), data = diversity_stability_synchrony)
 summary(synch_stab_mod)
-Anova(synch_stab_mod)
+car::Anova(synch_stab_mod)
 #                     Chisq Df Pr(>Chisq)    
 # synchrony         1308.34  1  < 2.2e-16 ***
 # habitat            103.13  3  < 2.2e-16 ***
@@ -66,7 +73,7 @@ Anova(synch_stab_mod)
 hist(residuals(synch_stab_mod)) # looks good
 plot(residuals(synch_stab_mod) ~ fitted(synch_stab_mod)) # same issue as always
 r.squaredGLMM(synch_stab_mod) # 0.63, 0.71
-performance::r2(synch_stab_mod) # 0.66, 0.74
+performance::r2(synch_stab_mod) # marginal: 0.66, conditional: 0.74
 emtrends(synch_stab_mod, pairwise ~ habitat, var = "synchrony") # fringing and backreef not different
 # contrast                    estimate    SE  df z.ratio p.value
 # Fringing - Backreef            0.181 0.107 Inf   1.695  0.3262
@@ -75,3 +82,55 @@ emtrends(synch_stab_mod, pairwise ~ habitat, var = "synchrony") # fringing and b
 # Backreef - Forereef 10m        1.311 0.157 Inf   8.346  <.0001
 # Backreef - Forereef 17m        2.255 0.161 Inf  14.008  <.0001
 # Forereef 10m - Forereef 17m    0.944 0.181 Inf   5.222  <.0001
+
+#pull out data from models to make supplemental figures:
+
+# get estimates for taxonomic richness
+t.s4.a <- as.tibble(emtrends(rich_stab_mod, pairwise ~ habitat, var = "richness_mean")$emtrends) %>% 
+  mutate(Predictor = "Taxonomic richness") %>% 
+  rename_if(str_detect(names(.), ".trend"), ~"Mean") %>%
+  mutate( #need to change letters by hand if post hoc contrasts change
+    Letter = case_when(
+      habitat == "Fringing" ~ "a",
+      habitat == "Backreef"  ~ "b",
+      habitat == "Forereef 10m"  ~ "b",
+      habitat == "Forereef 17m"  ~ "a"))
+
+# get estimates for fucntional richness
+t.s4.b <- as.tibble(emtrends(rich_stab_mod_fg, pairwise ~ habitat, var = "functional_richness_mean")$emtrends) %>% 
+  mutate(Predictor = "Functional richness") %>% 
+  rename_if(str_detect(names(.), ".trend"), ~"Mean") %>%
+  mutate( #need to change letters by hand if post hoc contrasts change
+    Letter = case_when(
+      habitat == "Fringing" ~ "a",
+      habitat == "Backreef"  ~ "b",
+      habitat == "Forereef 10m"  ~ "b",
+      habitat == "Forereef 17m"  ~ "b"))
+
+# get estimates for synchrony
+t.s4.c <- as.tibble(emtrends(synch_stab_mod, pairwise ~ habitat, var = "synchrony")$emtrends) %>% 
+  mutate(Predictor = "Synchrony") %>% 
+  rename_if(str_detect(names(.), ".trend"), ~"Mean") %>%
+  mutate( #need to change letters by hand if post hoc contrasts change
+    Letter = case_when(
+      habitat == "Fringing" ~ "a",
+      habitat == "Backreef"  ~ "a",
+      habitat == "Forereef 10m"  ~ "b",
+      habitat == "Forereef 17m"  ~ "c"))
+
+rbind(t.s4.a, t.s4.b, t.s4.c) %>% 
+  mutate(`Spatial scale` = "Plot-level", 
+         Table = "S4") %>% 
+  dplyr::select(-SE, -df) -> table_s4_plotlevel
+
+table_s4_plotlevel %>% 
+  # rename to match previous version/code
+  dplyr::rename(Habitat = habitat,
+                Lower_CI = asymp.LCL,
+                Upper_CI = asymp.UCL) %>%
+  # make a blank column to fill in the letter designations manually
+  # save as CSV
+  write_csv(here::here("data/supplemental_tables_tableS4_plotlevel.csv"))
+
+
+
