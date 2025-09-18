0b3765e6f2fa751127529533a31b814c9767dae3
Lauren Enright <enrightl09@gmail.com>
2025-09-18 13:24:05 -0700

    changing flow of data qaqc and data prep to write out 1 dataframe that will be uploaded to EDI as our data package. scripts 1a and 1b will not be a part of the scripts we publish with the paper.

--- 01a_data_qaqc.R
+++ 01a_data_qaqc.R
@@ -11,20 +11,30 @@ library(effects)
 library(devtools)
 library(here)
 
-data <- read.csv(here::here("data", "MCR_LTER_Annual_Survey_Benthic_Cover_20231211.csv"), stringsAsFactors = F) # replacements don't work when strings are factors
+#read in raw data from EDI 
+data_edi <- read.csv(here::here("data", "MCR_LTER_Annual_Survey_Benthic_Cover_20231211.csv"), stringsAsFactors = F) # replacements don't work when strings are factors
 
+#make a copy of the EDI data to edit
+data <- data_edi
 str(data)
 
-levels(as.factor(data$Taxonomy_Substrate_Functional_Group)) # 86 levels
+data_edi %>%
+  filter(Location == "LTER 5 Fringing Reef Algae Transect 3 Quad 4") %>%
+  filter(Year == 2011)
+
+
+sort(unique(data_edi$Taxonomy_Substrate_Functional_Group)) 
+
+levels(as.factor(data_edi$Taxonomy_Substrate_Functional_Group)) # 86 levels
 data$Site_habitat <- as.factor( paste(data$Site, data$Habitat))
 
 #### REPLACE OUTDATED TAXA
 
 # Julianna notes on taxa:
-# Changed Caulerpa peltata to Caulerpa chemnitzia 
+# Changed Caulerpa peltata to Caulerpa chemnitzia  --> YES
 # Changed Chnoospora implexa to Pseudochnoospora implexa
 # Changed Neogoniolithon frutescens to Neogoniolithon brassica-florida
-# Changed Caulerpa pickeringii to Caulerpa webbiana
+# Changed Caulerpa pickeringii to Caulerpa webbiana --> YES
 # Changed Boodlea kaeneana to Cladophoropsis membranacea
 # Changed Dictyota divaricata to Dictyota cervicornis
 
@@ -37,16 +47,8 @@ data$Taxonomy_Substrate_Functional_Group[data$Taxonomy_Substrate_Functional_Grou
 
 data$Taxonomy_Substrate_Functional_Group[data$Taxonomy_Substrate_Functional_Group == "Dictyota divaricata"] <- "Dictyota cervicornis"
 
-# Caulerpa peltata is outdated --> Caulerpa racemosa JULIANNA UPDATE = Caulerpa chemnitzia
+# Caulerpa peltata is outdated --> Caulerpa chemnitzia
 data$Taxonomy_Substrate_Functional_Group[data$Taxonomy_Substrate_Functional_Group == "Caulerpa peltata"] <- "Caulerpa chemnitzia"
-
-
-### EVERYTHING ELSE BELOW AS BEFORE ###
-
-
-# Boodlea kaeneana --> Cladophoropsis membranacea
-data$Taxonomy_Substrate_Functional_Group[data$Taxonomy_Substrate_Functional_Group == "Boodlea kaeneana"] <- 
-  "Cladophoropsis membranacea"
 
 # Cladophoropsis luxurians --> Cladophora fuliginosa
 data$Taxonomy_Substrate_Functional_Group[data$Taxonomy_Substrate_Functional_Group == "Cladophoropsis luxurians"] <-
@@ -57,9 +59,50 @@ data$Taxonomy_Substrate_Functional_Group[data$Taxonomy_Substrate_Functional_Grou
   "Neogoniolithon brassica-florida"
 
 # remove old levels
-data <- droplevels(data)
-levels(as.factor(data$Taxonomy_Substrate_Functional_Group)) # 84 levels --> 85 AFTER JULIANNA UPDATES
+data <- droplevels(data) 
+#which levels do we lose --> none, this is to remove potential ghost categories from the renames.
 
+levels(as.factor(data_edi$Taxonomy_Substrate_Functional_Group))
+#data from original EDI has 86 levels
+
+levels(as.factor(data$Taxonomy_Substrate_Functional_Group)) # 85 AFTER JULIANNA UPDATES
+#new data has 85 levels. This is because we changed "Boodlea kaeneana" to Cladophoropsis membranacea, 
+#and Cladophoropsis membranacea was already in the original EDI data
+
+data_edi %>%
+  filter(Taxonomy_Substrate_Functional_Group == "Cladophoropsis membranacea") %>%
+  tally()
+
+data_edi %>%
+  filter(Taxonomy_Substrate_Functional_Group == "Boodlea kaeneana") %>%
+  tally()
+
+#1449 --> Cladophoropsis membranacea
+#450 --> "Boodlea kaeneana"
+
+data %>%
+  filter(Taxonomy_Substrate_Functional_Group == "Cladophoropsis membranacea") %>%
+  tally()
+#1899, yep because 1449 + 450 = 1899
+
+setdiff(data_edi$Taxonomy_Substrate_Functional_Group, data$Taxonomy_Substrate_Functional_Group)
+
+#[1] "Caulerpa peltata"          "Chnoospora implexa"        "Cladophoropsis luxurians" 
+#[4] "Neogoniolithon frutescens" "Caulerpa pickeringii"      "Boodlea kaeneana"         
+#[7] "Dictyota divaricata" 
+
+setdiff(data$Taxonomy_Substrate_Functional_Group, data_edi$Taxonomy_Substrate_Functional_Group)
+
+#[1] "Caulerpa chemnitzia"             "Pseudochnoospora implexa"        "Cladophora fuliginosa"          
+#[4] "Neogoniolithon brassica-florida" "Caulerpa webbiana"               "Dictyota cervicornis" 
+
+# Changed Caulerpa peltata to Caulerpa chemnitzia 
+# Changed Chnoospora implexa to Pseudochnoospora implexa
+# Changed Cladophoropsis luxurians to Cladophora fuliginos
+# Changed Neogoniolithon frutescens to Neogoniolithon brassica-florida
+# Changed Caulerpa pickeringii to Caulerpa webbiana
+# Changed Boodlea kaeneana to Cladophoropsis membranacea 
+# Changed Dictyota divaricata to Dictyota cervicornis
 
 quad_level_summary <- data %>%
   dplyr::group_by(Year, Location, Site, Habitat, Site_habitat, Transect, Quadrat) %>%
@@ -67,7 +110,8 @@ quad_level_summary <- data %>%
 
 non100_quad_level <- quad_level_summary %>%
   dplyr::filter(sum != 100)
-
+  
+unique(data$Habitat)
 
 # can see that there are 99 observations that do not equal 100
 dim(non100_quad_level)
@@ -95,21 +139,32 @@ unique(quad_level_summary$sum)
 
 #write.csv(mistakes_quad_level, "mistakes_quad_level.csv", row.names = FALSE)
 
-
 ### CREATE DATAFRAME THAT IS JUST ALGAE
 
-not_algae <- c("Ascidian", "Bare Space", "Coral", "Coral Rubble", "Corallimorpharia", "Heteractis sp.", "Millepora platyphylla", "No data", "Sand", "Sarcophyton sp.", "Shell Debris", "Soft Coral", "Sponge", "Tridacna sp.", "Zooxanthellae")
+#not_algae <- c("Ascidian", "Bare Space", "Coral", "Coral Rubble", "Corallimorpharia", "Heteractis sp.", "Millepora platyphylla", "No data", "Sand", "Sarcophyton sp.", "Shell Debris", "Soft Coral", "Sponge", "Tridacna sp.", "Zooxanthellae")
 
 ### CREATE DATAFRAME THAT IS JUST MACROALGAE
 # Maintaining mat-forming cyanos even though they're not macroalgae in the traditional sense
 # "Coelothrix irregularis", "Symploca hydnoides" 
 
-not_macro <- c("Algal Turf", "Crustose Corallines", "Cyanophyta", "Damselfish Turf", "Lithophyllum kotschyanum", "Neogoniolithon brassica-florida", "Sporolithon sp." )
+#not_macro <- c("Algal Turf", "Crustose Corallines", "Cyanophyta", "Damselfish Turf", "Lithophyllum kotschyanum", "Neogoniolithon brassica-florida", "Sporolithon sp." )
 
 
-comb_wide <- cast(data, Year + Location + Site + Habitat + Site_habitat + Transect + Quadrat ~ Taxonomy_Substrate_Functional_Group, fun.aggregate = mean, value = "Percent_Cover") # this is the correct number of rows given the missing data
+comb_wide <- reshape::cast(data, Year + Location + Site + Habitat + Site_habitat + Transect + Quadrat ~ Taxonomy_Substrate_Functional_Group, fun.aggregate = mean, value = "Percent_Cover") # this is the correct number of rows given the missing data
+colnames(data)
 
-#21000 obs of 91 variables
+#combwide --> 21000 obs of 92 variables
+
+#test_old <- read.csv(here::here("data", "allbenthicdata_cleaned.csv"))
+#this old, but has 92 columns so I think the old code notes about 91 are just wrong?
+#has only 19701 obserbations 
+test_old2 <- read.csv(here::here("data", "allbenthicdata_cleaned_colnames.csv"))
+#has 19750  obserbations 
+#10 columns 
+#7 are meta data 
+
+colnames(test_old2)
+
 
 #change NAs to 0s 
 
@@ -118,33 +173,41 @@ is.nan.data.frame <- function(x){
 
 comb_wide[is.nan.data.frame(comb_wide)] <- 0
 
-#21000 obs of 91 variables
+#21000 obs of 92 again, 92 variables now
+
+colnames(comb_wide)
+
 
 #### QAQC WIDE DATAFRAME ####
 
 comb_wide_quad_sum <- comb_wide %>%
-  mutate(row_sums = rowSums(dplyr::select(., 8:91)))
+  mutate(row_sums = rowSums(dplyr::select(., 8:92)))
 
 #do we have data for all transects?
 
 comb_wide_quad_sum %>%
   dplyr::group_by(Year, Site, Habitat) %>%
-  dplyr::summarise(count = n()) # 50 quadrats per habitat/year/site (this is correct)
+  dplyr::summarise(count = n()) %>%
+  dplyr::filter(count != 50)  # 50 quadrats per habitat/year/site (this is correct)
+ 
+#yay, 0
 
 comb_wide_quad_sum %>%
   dplyr::group_by(Year, Site) %>%
-  dplyr::summarise(count = n()) 
+  dplyr::summarise(count = n()) %>%
+  dplyr::filter(count != 200) 
 
 #should be 200 per year per site (50 at each habitat * 4 habitats)
+#18 that are not --> 
 #2005 and 2006 are one large collective survey
 #2021 sites are at 100 (did not do forereef habitats due to COVID)
 
 #do all quads = 100 ?
 
 non100_wide_quad_level <- comb_wide_quad_sum %>%
-  dplyr::filter(row_sums != 100)
+  dplyr::filter(row_sums != 100) 
 
-#also 99 observations that do not equal 100, which matches what we found in the long data
+#also 99 observations that do not equal 100, which matches what we found in the long data :) 
 
 #how many are no data (-1)
 comb_wide_quad_sum %>%
@@ -155,14 +218,15 @@ comb_wide_quad_sum %>%
 # grab the mistake quadrats 
 mistakes_wide_quad_level <- comb_wide_quad_sum %>%
   dplyr::filter(row_sums != 100) %>%
-  dplyr::filter(row_sums != -1)
+  dplyr::filter(row_sums != -1) #these are from no data 
+
+##remaining 85 have something weird happening! Will send to Bob for updates. For now, will filter them out.
 
 comb_wide_quad_sum %>%
   dplyr::filter(row_sums != 100) %>% 
   dplyr::filter(Year == 2020) %>% dim()
 
-#remaining 85 have something weird happening! Will send to Bob for updates. For now, 
-# will filter these out
+#looking just at 2020 ones ^^  --> 23 
 
 #want to make sure these 85 are the same we found using the long data
 
@@ -170,13 +234,18 @@ setdiff(mistakes_wide_quad_level$Location, mistakes_quad_level$Location)
 
 setdiff(mistakes_quad_level$Location, mistakes_wide_quad_level$Location)
 
+#no differences, YAY!
+
 #### DEAL WITH 99s IN WIDE DATAFRAME ####
 #LE talked to Hillary and we found that of the 85 rows that have sums that do not = 100, 49 of them are because a "-1" was accidentally added to the no data column, even though data was taken. (100 percent cover - 1 = 99). This happened only in 2017. Hillary will fix this eventually on EDI, but for now I am going to manually remove these -1 from the data set so we can continue with our analyses 
 
 #show this error in the data
 
+
 no_data_mistakes <- mistakes_wide_quad_level %>%
   filter(row_sums == 99)
+
+#yep, 49 obvs
 
 unique(no_data_mistakes$`No data`)
 
@@ -191,19 +260,31 @@ cleaned_sumrows99 <- comb_wide_quad_sum %>%
 
 unique(cleaned_sumrows99$`No data`)
 
+comb_wide_quad_sum %>%
+  filter(row_sums == 99) %>% dim()
+#49
+         
+#this is 49. 
+
+#need to add these "fixed" quads back in 
+comb_wide_quad_sum_final <- comb_wide_quad_sum %>%
+  dplyr::select(-row_sums) %>% #remove old columns
+  mutate(row_sums_final = rowSums(dplyr::select(., 8:92)))
+
 #### FILTER RAMP UP YEARS ####
 
 #we want to filter out 2005 and 2006 - these were "ramp up years"
 #also want to filter out rows where the sum does not = 0 
 wide_alldata_filtered <- comb_wide_quad_sum_final %>%
   dplyr::filter(Year != 2005 & Year != 2006) %>%
-  dplyr::filter(row_sums == 100)
+  dplyr::filter(row_sums_final == 100)
 
-#should be 19750 obs
+#should be 19750 obs --> yay! This is with "99" quads added back in!! 
 dim(wide_alldata_filtered)
 
+
 #writing one file so column names will work with Noam's code
-write.csv(wide_alldata_filtered, "data/allbenthicdata_cleaned.csv", row.names = FALSE)
+#write.csv(wide_alldata_filtered, "data/allbenthicdata_cleaned.csv", row.names = FALSE)
 
 #clean names so column names are not so hard to work with
 
@@ -217,48 +298,7 @@ wide_alldata_filtered_cleaned_column_names <-
          site_habitat = gsub(" ", "_", str_to_lower(site_habitat)),
   ) 
 
+#this is the only csv we need to write out of this script as of september 15, 2025
+write.csv(wide_alldata_filtered_cleaned_column_names, "data/allbenthicdata_cleaned_colnames_09162025.csv", row.names = FALSE)
 
-write.csv(wide_alldata_filtered_cleaned_column_names, "data/allbenthicdata_cleaned_colnames.csv", row.names = FALSE)
 
-#we also want a copy that only includes taxa we care about 
-#not_algae character string created by Noam in Chunk 4 (Subsetting the data) and includes 14 
-#categories  -- these categories will be listed in a separate document as well 
-
-wide_algaeonly_filtered <- wide_alldatsa_filtered[ , -which(names(wide_alldata_filtered) 
-                                                            %in% not_algae)]
-
-#need to still remove the row_sums column for algae only 
-
-wide_algaeonly_filtered <- wide_algaeonly_filtered %>%
-  dplyr::select(-row_sums)
-
-write.csv(wide_algaeonly_filtered, "algaeonly_cleaned.csv", row.names = FALSE)
-
-#clean names so column names are not so hard to work with
-
-wide_algaeonly_cleaned_column_names <- 
-  wide_algaeonly_filtered %>%
-  # clean column names
-  janitor::clean_names() %>% 
-  # clean the inside as well
-  mutate(location = gsub(" ", "_", str_to_lower(location)),
-         site = gsub(" ", "_", str_to_lower(site)),
-         habitat = gsub(" ", "_", str_to_lower(habitat)),
-         site_habitat = gsub(" ", "_", str_to_lower(site_habitat)),
-  ) 
-
-write.csv(wide_algaeonly_cleaned_column_names, "data/algaeonly_cleaned_colnames.csv", row.names = FALSE)
-
-#### FILTERED DATA QAQC ####
-# you can use this to see which sites lost quads during filtering process 
-#should be 50 per year/site/habitat combo
-
-wide_alldata_filtered %>%
-  dplyr::group_by(Year, Site, Habitat) %>%
-  dplyr::summarise(count = n())
-
-#should be 200 per year/site combo
-
-wide_alldata_filtered %>%
-  dplyr::group_by(Year, Site) %>%
-  dplyr::summarise(count = n())
--- 01b_data_prep.R
+++ 01b_data_prep.R
@@ -6,7 +6,9 @@ library(vegan)
 source("00_functions_and_aes.R")
 
 #### MACROALGAE DATA ####
-data <- read.csv(here::here("data", "allbenthicdata_cleaned_colnames.csv"), stringsAsFactors = F) 
+
+## read in data from script 01a
+wide_alldata_cleaned <- read.csv(here::here("data", "allbenthicdata_cleaned_colnames_09162025.csv"), stringsAsFactors = F) 
 
 ### if we want to make a missing or faulty data table
 # length(unique(data$location))
@@ -16,26 +18,50 @@ data <- read.csv(here::here("data", "allbenthicdata_cleaned_colnames.csv"), stri
 ###
 
 # remove remaining backreef and fringing 2020 data because of covid extrapolation
-filtered_data <- data %>%
+filtered_data <- wide_alldata_cleaned %>%
   filter(!(year == 2020 & habitat %in% c("fringing", "backreef")))
+#19173
 
 # re-factor for figures
 filtered_data$habitat <- factor(filtered_data$habitat, 
                                 levels = c("fringing", "backreef", "outer_10","outer_17"),
                                 labels = c("Fringing", "Backreef", "Forereef 10m", "Forereef 17m"))
 
-# note non-algae species
-not_algae <- c("ascidian", "bare_space", "coral", "coral_rubble", "corallimorpharia", "heteractis_sp", "millepora_platyphylla", "no_data", "sand", "sarcophyton_sp", "shell_debris", "soft_coral", "sponge", "tridacna_sp", "zooxanthellae", "row_sums")
+colnames(filtered_data)
+#okay - here is the deal:
+# filtered_data has 93 columns. 7 of them are meta data ("year", "location", "site", "habitat", "site_habitat", "transect", "quadrat")
+  # and one is "row_sums_final" --> so, 93 - 7 - 1 = 85.
+  # there were 86 taxa in the original EDI dataset, but we changed "Boodlea kaeneana" to Cladophoropsis membranacea, 
+  #and Cladophoropsis membranacea was already in the original EDI data, they combined down, so 85 total taxa. Yay!
 
+
+
+
+# note non-algae species
+not_algae <- c("ascidian", "bare_space", "coral", "coral_rubble", "corallimorpharia", "heteractis_sp", "millepora_platyphylla", "no_data", "sand", "sarcophyton_sp", "shell_debris", "soft_coral", "sponge", "tridacna_sp", "zooxanthellae", "row_sums_final")
+
+#15 taxa in non algae (16 values in not_algae vector, but one is "row_sums_final)
 
 # remove
 algae <- filtered_data[ , -which(names(filtered_data) %in% not_algae)]
 
+# did the filter work? 93 -16 = 77 = good. 
+#algae dataframe has 77 variables. 7 of these are meta data, which means 70 of them are algae. We had
+# 85 taxa, and removed 15, so, there should be 70 other columns. 70+7 = 77 = YAY! 
+
+
+#macroalgae
+#meta = 7 --> 70 - 7 = 63
+#was 62. 
 
 
 macroalgae <- algae %>% 
-  select(-algal_turf, -crustose_corallines, -cyanophyta, -damselfish_turf, -lithophyllum_kotschyanum,
+  dplyr::select(-algal_turf, -crustose_corallines, -cyanophyta, -damselfish_turf, -lithophyllum_kotschyanum,
          -neogoniolithon_brassica_florida, -sporolithon_sp)
+
+# did the filter work? 77-7 = 70 = good. 
+#macroalgae dataframe has 70 variables. 7 of these are meta data, which means 63 of them are algae. We had
+# 85 taxa, and removed 15, and now removed 7 more, so, there should be 63 other columns. 63+7 = 70 = YAY! 
 
 meta_cols <- c("year", "location", "site", "habitat", "site_habitat", "transect", "quadrat")
 
@@ -52,19 +78,58 @@ macro_long_data <- pivot_longer(
   names_to = "taxa",
   values_to = "percent_cover") %>% 
   mutate(prop_cover = percent_cover/100) %>% 
-  select(-percent_cover)
+  dplyr::select(-percent_cover)
 
-# aggregate for site-level analyses
+#new:1207899
+#old:1188726
+#difference is #19173
+#cool, I think that checks out bc 50 quads * 6 sites * 4 habitats = 1200 * 16 years = 19200 & we know we are missing a few... 
 
-site_macro <- macroalgae %>%
-  dplyr::group_by(year, site, habitat,site_habitat) %>%
-  dplyr::summarize(across(acanthophora_spicifera:valonia_ventricosa, \(x) mean(x, na.rm = TRUE))) 
+#NOW SWITCHING THE SCRIPT ORDER, NEED TO ADD FG species to get a dataframe to give to Hillary, then ALL analyses need to come from this dataframe.. 
 
-site_meta_cols <- c("year", "site", "habitat", "site_habitat")
-site_meta_df <- site_macro[,which(names(site_macro) %in% meta_cols)]
+algae_fg_raw <- read_csv(here::here("data","MCR_Algal_Functional_Groups_09132025_updatedtax_v4.csv"))
+
+#clean column names
+algae_fg <- algae_fg_raw %>%
+  janitor::clean_names()
+
+#change species names to be lowercase & have underscore 
+algae_fg$species <- gsub(" ", "_", tolower(algae_fg$species))
+algae_fg$morphology <- trimws(algae_fg$functional_grouping_updated_08202025)
+algae_fg$morphology <- gsub(" ", "_", algae_fg$morphology)
+
+#rename species column to taxa
+
+algae_fg <- algae_fg %>%
+  dplyr::select(species, morphology) %>%
+  dplyr::rename(taxa = species) %>%
+  dplyr::rename(functional_group = morphology)
+
+unique(algae_fg$functional_group) # 9 functional groups
+unique(algae_fg$taxa) #63 taxa
+
+#merge functional groups  
+macro_functional_groups <- merge(macro_long_data, algae_fg, by = "taxa", all.x = TRUE)
+#yay, same amount of observations + 1 variable 
+
+
+unique(macro_functional_groups$taxa) #63
+unique(macro_functional_groups$functional_group) #9
+
+fg_NAs_QAQC <- macro_functional_groups %>%
+  filter(is.na(functional_group))
+
+#yay, 0 
+
+#THIS IS THE DATA WE WANT TO WRITE OUT - THIS WILL GO ON EDI
+
+#write.csv(macro_functional_groups, "data/macroalgalfunctionalgroups_long_09162025.csv", row.names = FALSE)
 
 #### GENERAL COVER DATAFRAME ####
 # For figures 1 and merging with figure 2
+
+### We also want to make a nice, summarized "cover_df" dataframe, using our filtered data from above.
+
 cover_df <- data.frame(
   year = filtered_data$year,
   habitat = filtered_data$habitat,
@@ -91,6 +156,36 @@ cover_df <- data.frame(
 
 cover_df$total <- rowSums(cover_df[,-which(names(cover_df) %in% meta_cols)])
 
+cover_df %>%
+  filter(total != 100) #nice 
+
+
+#write this out... 
+#write.csv(cover_df, "data/cover_df_09162025.csv", row.names = FALSE)
+
+
+
+#now, i think we need to flip long data to wide
+
+
+taxa_sum_check <- macro_functional_groups %>%
+  dplyr::group_by(location, year) %>%
+  dplyr::summarise(sum_macrocover = sum(prop_cover))
+
+macro_functional_groups %>%
+  filter(location == "lter_2_backreef_algae_transect_1_quad_1") %>%
+  filter(year == "2022")
+
+# aggregate for site-level analyses
+
+site_macro <- macroalgae %>%
+  dplyr::group_by(year, site, habitat,site_habitat) %>%
+  dplyr::summarize(across(acanthophora_spicifera:valonia_ventricosa, \(x) mean(x, na.rm = TRUE))) 
+
+site_meta_cols <- c("year", "site", "habitat", "site_habitat")
+site_meta_df <- site_macro[,which(names(site_macro) %in% meta_cols)]
+
+
 #### CALCULATE DIVERSITY METRICS ####
 alpha_diversity_quad_macro <-
   data.frame(
@@ -107,7 +202,7 @@ macroalgal_cover_quad <- cover_df[,c("year", "habitat", "site", "location", "mac
 alpha_diversity_quad_macro <-
   left_join(alpha_diversity_quad_macro, macroalgal_cover_quad,by = join_by(location, year, site, habitat)) %>% 
   mutate(macroalgal_prop_cover = macroalgae / 100) %>% 
-  select(-macroalgae)
+  dplyr::select(-macroalgae)
 
 alpha_diversity_quad_macro$cover_trans <- sv_trans(alpha_diversity_quad_macro$macroalgal_prop_cover)
 
@@ -127,43 +222,13 @@ alpha_diversity_site_macro$percent_cover <- rowSums(site_macro[,-which(names(sit
 alpha_diversity_site_macro$prop_cover <- alpha_diversity_site_macro$percent_cover/100
 
 alpha_diversity_site_macro$cover_trans <- sv_trans(alpha_diversity_site_macro$prop_cover)
-#### repeat for functional group data ####
-algae_fg_raw2 <- read_csv(here::here("data","MCR_Algal_Functional_Groups_08202025_updatedtax_v3.csv"))
 
 
-#clean column names
-algae_fg <- algae_fg_raw2 %>%
-  janitor::clean_names()
-
-#change species names to be lowercase & have underscore 
-algae_fg$species <- gsub(" ", "_", tolower(algae_fg$species))
-algae_fg$morphology <- trimws(algae_fg$functional_grouping_updated_08202025)
-algae_fg$morphology <- gsub(" ", "_", algae_fg$morphology)
-
-#rename species column to taxa
-
-algae_fg <- algae_fg %>%
-  select(species, morphology) %>%
-  dplyr::rename(taxa = species) %>%
-  dplyr::rename(functional_group = morphology)
-
-unique(algae_fg$functional_group)
-
-#merge functional groups  
-macro_functional_groups <- merge(macro_long_data, algae_fg, all.x = TRUE)
-
-taxa_sum_check <- macro_functional_groups %>%
-  dplyr::group_by(location, year) %>%
-  dplyr::summarise(sum_macrocover = sum(prop_cover))
-
-macro_functional_groups %>%
-  filter(location == "lter_2_backreef_algae_transect_1_quad_1") %>%
-  filter(year == "2022")
 
 #summarizing functional groups at the quad level
 fg_summary <- macro_functional_groups %>%
   dplyr::group_by(location, year, functional_group, habitat, site) %>%
-  dplyr::summarise(sum_fg_cover = sum(prop_cover))
+  dplyr::summarise(sum_fg_cover = sum(prop_cover)) 
 
 max(fg_summary$sum_fg_cover)
 
@@ -194,8 +259,9 @@ check_them %>%
 fg_summary_wide <- fg_summary %>%
   tidyr::pivot_wider(names_from = "functional_group", values_from = "sum_fg_cover")
 
+colnames(fg_summary_wide)
 #richness at every quad/year combination 
-fg_summary_wide$functional_richness <- vegan::specnumber(fg_summary_wide[5:14])
+fg_summary_wide$functional_richness <- vegan::specnumber(fg_summary_wide[5:13])
 
 unique(fg_summary_wide$functional_richness)
 #0 to 4
@@ -206,7 +272,7 @@ fg_summary_site_wide_SUM <- fg_summary_site_SUM %>%
   pivot_wider(names_from = "functional_group", values_from = "sum_fg_cover")
 
 #richness at every site/year combination ---> NOT AVERAGED,
-fg_summary_site_wide_SUM$functional_richness <- vegan::specnumber(fg_summary_site_wide_SUM[4:13])
+fg_summary_site_wide_SUM$functional_richness <- vegan::specnumber(fg_summary_site_wide_SUM[4:12])
 
 
 #merge Noam's data with functional group summary 
@@ -214,11 +280,17 @@ alpha_diversity_quad_macro <- merge(alpha_diversity_quad_macro,
                                     fg_summary_wide[,c("location", "year", "site",  "habitat", "functional_richness")],
                                     by = c("location", "year", "site", "habitat"))
 
+alpha_diversity_quad_macro %>%
+  dplyr::select(location, year, richness, functional_richness) %>%
+  dplyr::filter(location == "lter_1_backreef_algae_transect_1_quad_1")
+
 
 #merge site level cover with site functional group richness
 alpha_diversity_site_macro<-merge(alpha_diversity_site_macro,
                                   fg_summary_site_wide_SUM[,c("year", "site",  "habitat", "functional_richness")],
                                   by = c("year", "site",  "habitat"))
 
-#write.csv(alpha_diversity_quad_macro, "data/alpha_diversity_quad_macro.csv", row.names = FALSE)
-#write.csv(alpha_diversity_site_macro, "data/alpha_diversity_site_macro.csv", row.names = FALSE)
+write.csv(alpha_diversity_quad_macro, "data/alpha_diversity_quad_macro.csv", row.names = FALSE)
+write.csv(alpha_diversity_site_macro, "data/alpha_diversity_site_macro.csv", row.names = FALSE)
+
+
