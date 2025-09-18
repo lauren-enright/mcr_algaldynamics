5707afddb630a5ade740c6f5cc5cb8981f1def19
Lauren Enright <enrightl09@gmail.com>
2025-09-18 13:29:12 -0700

    changing flow - read in dataframes from script 04a, adding supplemental figures of model output

--- 04b_dss_models.R
+++ 04b_dss_models.R
@@ -6,31 +6,46 @@ library(effects)
 library(DHARMa)
 library(codyn)
 library(multcomp)
+library(here)
 
-source("04a_calculate_synchrony.R")
+source("00_functions_and_aes.R")
+#source("04a_calculate_synchrony.R")
 
 #read in data produced in 04a_calculate_synchrony script
-#diversity_stability_synchrony <- read.csv(here::here("data", "full_plot_level_dss.csv")) 
-#diversity_stability_synchrony_site <- read.csv(here::here("data", "diversity_stability_synchrony_site.csv"))
+diversity_stability_synchrony <- read.csv(here::here("data", "full_plot_level_dss_09172025.csv")) 
+diversity_stability_synchrony_site <- read.csv(here::here("data", "diversity_stability_synchrony_site_09172025.csv"))
 
 #want habitats to be in order.
 diversity_stability_synchrony$habitat <- factor(diversity_stability_synchrony$habitat,
                                              levels = c("Fringing", "Backreef", "Forereef 10m", "Forereef 17m"))
 
+# EXTRACT RANGES
+dss_ranges <- extract_ranges(diversity_stability_synchrony, "habitat",
+                             c("richness_mean", "functional_richness_mean", "cover_stability", "synchrony"))
+
+
 #### STABILITY ~ RICHNESS ####
 rich_stab_mod <- glmmTMB(cover_stability~ richness_mean*habitat + (1|site), family = Gamma(link = "log"), data = diversity_stability_synchrony)
 summary(rich_stab_mod)
 car::Anova(rich_stab_mod)
+#NOAM
 #                          Chisq Df Pr(>Chisq)    
 # richness_mean         2720.311  1  < 2.2e-16 ***
 # habitat                325.657  3  < 2.2e-16 ***
 # richness_mean:habitat   33.857  3  2.124e-07 ***
+#LAUREN
+#                        Chisq Df Pr(>Chisq)    
+#richness_mean         2721.440  1  < 2.2e-16 ***
+#habitat                325.876  3  < 2.2e-16 ***
+#richness_mean:habitat   33.926  3  2.054e-07 ***
+
 hist(residuals(rich_stab_mod)) # looks good
 plot(residuals(rich_stab_mod) ~ fitted(rich_stab_mod)) # heteroskedastic - bring to group
-r.squaredGLMM(rich_stab_mod) # 0.803, 0.808
-performance::r2(rich_stab_mod) # marginal: 0.812, conditional: 0.816
+#r.squaredGLMM(rich_stab_mod) # 0.803, 0.808
+performance::r2(rich_stab_mod) # marginal: 0.812, conditional: 0.816 - yes, matches
 em_rich_stab_mod <- emtrends(rich_stab_mod, pairwise ~ habitat, var = "richness_mean") # fringing not diff from outer17, backreef not different from outer10
 # $contrasts
+#NOAM
 #  contrast                     estimate     SE  df z.ratio p.value
 #  Fringing - Backreef          0.186640 0.0414 Inf   4.511  <.0001
 #  Fringing - Forereef 10m      0.187248 0.0455 Inf   4.114  0.0002
@@ -38,6 +53,15 @@ em_rich_stab_mod <- emtrends(rich_stab_mod, pairwise ~ habitat, var = "richness_
 #  Backreef - Forereef 10m      0.000608 0.0405 Inf   0.015  1.0000
 #  Backreef - Forereef 17m     -0.159158 0.0411 Inf  -3.875  0.0006
 #  Forereef 10m - Forereef 17m -0.159766 0.0439 Inf  -3.640  0.0016
+#LAUREN
+#contrasts
+#contrast                     estimate     SE  df z.ratio p.value
+#Fringing - Backreef          0.186671 0.0414 Inf   4.512  <.0001
+#Fringing - Forereef 10m      0.187196 0.0455 Inf   4.114  0.0002
+#Fringing - Forereef 17m      0.027111 0.0463 Inf   0.586  0.9364
+#Backreef - Forereef 10m      0.000525 0.0405 Inf   0.013  1.0000
+#Backreef - Forereef 17m     -0.159559 0.0411 Inf  -3.885  0.0006
+#Forereef 10m - Forereef 17m -0.160084 0.0439 Inf  -3.647  0.0015
 
 cld_rich_stab_mod <- multcomp::cld(em_rich_stab_mod, Letters = letters, sort = FALSE)
 
@@ -47,14 +71,19 @@ rich_stab_mod_fg <- glmmTMB(cover_stability ~ functional_richness_mean*habitat +
 summary(rich_stab_mod_fg)
 car::Anova(rich_stab_mod_fg)
 
-#                                    Chisq Df Pr(>Chisq)    
+#NOAM                                    Chisq Df Pr(>Chisq)    
 # functional_richness_mean         3077.168  1  < 2.2e-16 ***
 # habitat                           120.732  3  < 2.2e-16 ***
 # functional_richness_mean:habitat   22.582  3  4.935e-05 ***
+#LAUREN
+#Chisq Df Pr(>Chisq)    
+#functional_richness_mean         3099.815  1  < 2.2e-16 ***
+#habitat                           120.580  3  < 2.2e-16 ***
+#functional_richness_mean:habitat   25.078  3  1.487e-05 ***
 
 hist(residuals(rich_stab_mod_fg)) # looks fine
 plot(residuals(rich_stab_mod_fg) ~ fitted(rich_stab_mod_fg)) # same as usual 
-performance::r2(rich_stab_mod_fg) # marginal: 0.823, conditional: 0.832
+performance::r2(rich_stab_mod_fg) # marginal: 0.823, conditional: 0.832, matches 
 em_rich_stab_mod_fg <- emtrends(rich_stab_mod_fg, pairwise ~ habitat, var = "functional_richness_mean") 
 #  fringing different from all others
 # contrast                    estimate     SE  df z.ratio p.value
@@ -64,6 +93,14 @@ em_rich_stab_mod_fg <- emtrends(rich_stab_mod_fg, pairwise ~ habitat, var = "fun
 #  Backreef - Forereef 10m       0.0501 0.0472 Inf   1.061  0.7131
 #  Backreef - Forereef 17m       0.0142 0.0440 Inf   0.324  0.9883
 #  Forereef 10m - Forereef 17m  -0.0358 0.0476 Inf  -0.753  0.8756
+#LAUREN
+#contrast                    estimate     SE  df z.ratio p.value
+#Fringing - Backreef           0.1829 0.0466 Inf   3.921  0.0005
+#Fringing - Forereef 10m       0.2337 0.0517 Inf   4.520  <.0001
+#Fringing - Forereef 17m       0.1981 0.0488 Inf   4.058  0.0003
+#Backreef - Forereef 10m       0.0508 0.0471 Inf   1.079  0.7024
+#Backreef - Forereef 17m       0.0152 0.0439 Inf   0.346  0.9858
+#Forereef 10m - Forereef 17m  -0.0356 0.0475 Inf  -0.750  0.8767
 
 cld_rich_stab_mod_fg <- multcomp::cld(em_rich_stab_mod_fg, Letters = letters, sort = FALSE)
 
@@ -71,14 +108,21 @@ cld_rich_stab_mod_fg <- multcomp::cld(em_rich_stab_mod_fg, Letters = letters, so
 synch_stab_mod <- glmmTMB(cover_stability ~ synchrony*habitat + (1|site), family = Gamma("log"), data = diversity_stability_synchrony)
 summary(synch_stab_mod)
 car::Anova(synch_stab_mod)
+#NOAM
 #                     Chisq Df Pr(>Chisq)    
 # synchrony         1308.34  1  < 2.2e-16 ***
 # habitat            103.13  3  < 2.2e-16 ***
 # synchrony:habitat  327.96  3  < 2.2e-16 ***
+
+#LAUREN --> only different by 0.01 for synch and interaction.. 
+#Chisq Df Pr(>Chisq)    
+#synchrony         1308.33  1  < 2.2e-16 ***
+#  habitat            103.13  3  < 2.2e-16 ***
+#  synchrony:habitat  327.97  3  < 2.2e-16 ***
 hist(residuals(synch_stab_mod)) # looks good
 plot(residuals(synch_stab_mod) ~ fitted(synch_stab_mod)) # same issue as always
-r.squaredGLMM(synch_stab_mod) # 0.63, 0.71
-performance::r2(synch_stab_mod) # marginal: 0.66, conditional: 0.74
+#r.squaredGLMM(synch_stab_mod) # 0.63, 0.71
+performance::r2(synch_stab_mod) # marginal: 0.66, conditional: 0.74 - matches
 em_synch_stab_mod <- emtrends(synch_stab_mod, pairwise ~ habitat, var = "synchrony") # fringing and backreef not different
 # contrast                    estimate    SE  df z.ratio p.value
 # Fringing - Backreef            0.181 0.107 Inf   1.695  0.3262
@@ -87,6 +131,7 @@ em_synch_stab_mod <- emtrends(synch_stab_mod, pairwise ~ habitat, var = "synchro
 # Backreef - Forereef 10m        1.311 0.157 Inf   8.346  <.0001
 # Backreef - Forereef 17m        2.255 0.161 Inf  14.008  <.0001
 # Forereef 10m - Forereef 17m    0.944 0.181 Inf   5.222  <.0001
+#matches
 
 cld_synch_stab_mod <- multcomp::cld(em_synch_stab_mod, Letters = letters, sort = FALSE)
 
@@ -112,14 +157,10 @@ rbind(t.s4.a, t.s4.b, t.s4.c) %>%
          Table = "S4") %>% 
   dplyr::select(-SE, -df) -> table_s4_plotlevel
 
-table_s4_plotlevel %>% 
+supplemental_tables_tableS4_plot <- table_s4_plotlevel %>% 
   # rename to match previous version/code
   dplyr::rename(Habitat = habitat,
                 Lower_CI = asymp.LCL,
-                Upper_CI = asymp.UCL) %>%
-  # make a blank column to fill in the letter designations manually
-  # save as CSV
-  write_csv(here::here("data/supplemental_tables_tableS4_plotlevel.csv"))
-
+                Upper_CI = asymp.UCL)
 
 
--- 04c_dss_figures.R
+++ 04c_dss_figures.R
@@ -1,9 +1,10 @@
-source("04b_dss_models.R")
 library(ggplot2)
 library(ggpubr)
 library(patchwork)
 
-supplemental_tables_tableS4_plot <- read.csv(here::here("data", "supplemental_tables_tableS4_plotlevel.csv"))
+#require this for model output
+source("04b_dss_models.R")
+
 
 #### STABILITY ~ RICHNESS ####
 # SETUP
@@ -13,16 +14,27 @@ rich_stab_emm <- emmip(rich_stab_mod,
 
 filtered_rich_stab_effects <- filter_ranges(rich_stab_emm, dss_ranges, "habitat", "richness_mean")
 
+any(is.na(diversity_stability_synchrony$richness_mean))  #false
+sum(is.na(diversity_stability_synchrony$cover_stability)) #16
+any(is.infinite(diversity_stability_synchrony$richness_mean)) #false
+any(is.infinite(diversity_stability_synchrony$cover_stability)) #false
+
+diversity_stability_synchrony %>% filter(is.na(cover_stability))
+
+## NOAM --> is it right that there were only 16 quads through time that don't have any algae cover?
+#Warrning message:
+#Removed 16 rows containing missing values or values outside the scale range (`geom_point()`) --> these are NAs in the stability column
+
 # PLOT
-(figure_4a <- 
+(figure_3a <- 
     ggplot() +
     geom_point(data = diversity_stability_synchrony,
                aes(x = richness_mean, y = cover_stability, colour = habitat), size = 1, alpha = 0.5) + 
     geom_line(data = filtered_rich_stab_effects, aes(x = richness_mean, y = exp(yvar), colour = habitat), size = 1.5) +
     geom_ribbon(data = filtered_rich_stab_effects, aes(x = richness_mean, ymin = exp(LCL), ymax = exp(UCL), fill = habitat),
                 alpha = 0.3) +
-    scale_colour_manual(values = habitat_colours) +
-    scale_fill_manual(values = habitat_colours) +
+    scale_colour_manual(values = habitat_colours, labels = habitat_labels) +
+    scale_fill_manual(values = habitat_colours, labels = habitat_labels) +
     labs(y = "Stability\n", x = "Taxonomic richness", title = "") +
     model_themes
 )
@@ -37,8 +49,13 @@ rich_stab_emm_fg <- emmip(rich_stab_mod_fg,
 
 filtered_rich_stab_effects_fg <- filter_ranges(rich_stab_emm_fg, dss_ranges, "habitat", "functional_richness_mean")
 
+
+## NOAM --> is it right that there were only 16 quads through time that don't have any algae cover? 
+#Warrning message:
+#Removed 16 rows containing missing values or values outside the scale range (`geom_point()`). -->  these are NAs in the stability column
+
 # PLOT
-(figure_4b <- 
+(figure_3b <- 
     ggplot() +
     geom_point(data = diversity_stability_synchrony,
                aes(x = functional_richness_mean, y = cover_stability, colour = habitat), size = 1, alpha = 0.5) + 
@@ -46,8 +63,8 @@ filtered_rich_stab_effects_fg <- filter_ranges(rich_stab_emm_fg, dss_ranges, "ha
     geom_ribbon(data = filtered_rich_stab_effects_fg,
                 aes(x = functional_richness_mean, ymin = exp(LCL), ymax = exp(UCL), fill = habitat),
                 alpha = 0.3) +
-    scale_colour_manual(values = habitat_colours) +
-    scale_fill_manual(values = habitat_colours) +
+    scale_colour_manual(values = habitat_colours, labels = habitat_labels) +
+    scale_fill_manual(values = habitat_colours, labels = habitat_labels) +
     scale_x_continuous(breaks = c(0,1,2)) +
     labs(y = "", x = "Functional richness", title = "") +
     model_themes
@@ -64,22 +81,24 @@ synch_stab_emm <- emmip(synch_stab_mod,
 filtered_synch_stab_effects <- filter_ranges(synch_stab_emm, dss_ranges, "habitat", "synchrony")
 
 # PLOT
-(figure_4c <- 
+(figure_3c <- 
     ggplot() +
     geom_point(data = diversity_stability_synchrony,
                aes(x = synchrony, y = cover_stability, colour = habitat), size = 1, alpha = 0.5) + 
     geom_line(data = filtered_synch_stab_effects, aes(x = synchrony, y = exp(yvar), colour = habitat), size = 1.5) +
     geom_ribbon(data = filtered_synch_stab_effects, aes(x = synchrony, ymin = exp(LCL), ymax = exp(UCL), fill = habitat),
                 alpha = 0.3) +
-    scale_colour_manual(values = habitat_colours) +
-    scale_fill_manual(values = habitat_colours) +
+    scale_colour_manual(values = habitat_colours, labels = habitat_labels) +
+    scale_fill_manual(values = habitat_colours, labels = habitat_labels) +
     labs(x = "Synchrony", y = "", title = "") +
     model_themes
 )
 # ggsave(filename = "output/figure_4c.png", figure_4c, height = 10, width = 14)
 
+#same 16 rows removal warning... 
+
 #### PANEL GRAPH ####
-(figure_4 <- ggarrange(figure_4a, figure_4b, figure_4c, 
+(figure_3 <- ggarrange(figure_3a, figure_3b, figure_3c, 
                        ncol = 3, nrow = 1, 
                        common.legend = TRUE,
                        heights = c(1,1),
@@ -88,7 +107,7 @@ filtered_synch_stab_effects <- filter_ranges(synch_stab_emm, dss_ranges, "habita
                        labels = c("a.", "b.", "c.", "d.", "e.", "f."),
                        font.label = list(size = 26, color = "black", face = "plain")))
 
-# ggsave(filename = "output/figure_4v3.png", figure_4, height = 6, width = 18)
+#ggsave(filename = "output/figure_3_v4_09172025.png", figure_3, height = 6, width = 18)
 
 # Supplemental figure S4
 
@@ -103,7 +122,7 @@ supplemental_tables_tableS4_plot %>%
   # add confidence intervals:
   geom_linerange(aes(xmin = Lower_CI, xmax = Upper_CI), linewidth = 2.5) +
   #facet_wrap(~ Predictor) +
-  scale_colour_manual(values = habitat_colours) +
+  scale_colour_manual(values = habitat_colours, labels = habitat_labels) +
   xlab("Coefficient") +
   ylab("") +
   ggtitle("(a) Taxonomic richness") +
@@ -124,7 +143,7 @@ supplemental_tables_tableS4_plot %>%
   geom_point(size = 6) +
   # add confidence intervals:
   geom_linerange(aes(xmin = Lower_CI, xmax = Upper_CI), linewidth = 2.5) +
-  scale_colour_manual(values = habitat_colours) +
+  scale_colour_manual(values = habitat_colours, labels = habitat_labels) +
   xlab("Coefficient") +
   ylab("") +
   ggtitle("(b) Functional richness") +
@@ -145,7 +164,7 @@ supplemental_tables_tableS4_plot %>%
   geom_point(size = 6) +
   # add confidence intervals:
   geom_linerange(aes(xmin = Lower_CI, xmax = Upper_CI), linewidth = 2.5) +
-  scale_colour_manual(values = habitat_colours) +
+  scale_colour_manual(values = habitat_colours, label = habitat_labels) +
   xlab("Coefficient") +
   ylab("") +
   ggtitle("(c) Synchrony") +
@@ -158,7 +177,7 @@ supplemental_tables_tableS4_plot %>%
 s4_figure <- s4.taxon / s4.functional / s4.synch
 
 
-ggsave(filename = "output/figure_s4_plotlevel.png", s4_figure, height = 17, width = 13)
+#ggsave(filename = "output/figure_s4_plotlevel_v2_09172025.png", height = 17, width = 13)
 #if you make it narrower than 13 it cuts off the legend
 
 
